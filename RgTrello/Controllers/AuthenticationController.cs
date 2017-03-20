08d9e6eff81e1003530221905849849aa3deb154
Sebastián Villamonte <walden2av@gmail.com>
2017-03-20 21:56:38 +1300

    Added initial structure of the solution and got rid of all identified unnecessary things. Added a small link to sign in to Trello, without any behaviour yet.

---
+++ RgTrello/Controllers/AuthenticationController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace RgTrello.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        // Will handle going to Trello and recovering the token.
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RgTrello/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RgTrello.MvcApplication" Language="C#" %>
