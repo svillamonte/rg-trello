131f2b235ffafc665ffe1bc9a71f0dd5b3a254c5
Sebastián Villamonte <walden2av@gmail.com>
2017-03-26 11:37:46 +1300

    Added initial cards controller plus default view. This view is useless, so it prompts the user to go to the boards list first. Added only for consistency (and not to return a 404 error message).

---
+++ RgTrello.Tests/Controllers/CardsControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RgTrello.Controllers;
+using System.Web.Mvc;
+
+namespace RgTrello.Tests.Controllers
+{
+    [TestClass]
+    public class CardsControllerTests
+    {
+        private readonly CardsController _cardsController;
+
+        public CardsControllerTests()
+        {
+            _cardsController = new CardsController();
+        }
+
+        [TestMethod]
+        public void Index_ReturnsRelatedView()
+        {
+            // Arrange
+
+            // Act
+            var result = _cardsController.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ RgTrello/Controllers/CardsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace RgTrello.Controllers
+{
+    public class CardsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
