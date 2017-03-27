996fb06bba959f43cebdc4fb2d443122cd88936d
Sebastián Villamonte <walden2av@gmail.com>
2017-03-27 14:31:27 +1300

    Added tests to TrelloApiClient.

---
+++ RgTrello.Services.Tests/Trello/TrelloApiClientTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RgTrello.Services.Trello;
+
+namespace RgTrello.Services.Tests.Trello
+{
+    [TestClass]
+    public class TrelloApiClientTests
+    {
+        [TestMethod]
+        public void SetToken_AssignsInstanceOfIAuthenticator()
+        {
+            // Arrange
+            var token = "anicetoken";
+
+            var clientWithoutAuthenticator = new TrelloApiClient();
+            var clientWithAuthenticator = new TrelloApiClient();
+
+            // Act
+            clientWithAuthenticator.SetToken(token);
+
+            // Assert
+            Assert.IsNull(clientWithoutAuthenticator.Authenticator);
+            Assert.IsNotNull(clientWithAuthenticator.Authenticator);
+        }
+    }
+}
