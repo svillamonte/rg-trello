583c06707adb6e978c07c12dcdbe72e19395aa3d
Sebastián Villamonte <walden2av@gmail.com>
2017-03-27 14:23:44 +1300

    Added tests for TrelloAuthenticator.

---
+++ RgTrello.Services.Tests/Trello/TrelloAuthenticatorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RestSharp;
+using RgTrello.Services.Trello;
+
+namespace RgTrello.Services.Tests.Trello
+{
+    [TestClass]
+    public class TrelloAuthenticatorTests
+    {
+        [TestMethod]
+        public void Authenticate_CallsRestRequestWithConstructorParametersValues()
+        {
+            // Arrange
+            var applicationKey = "aniceapplicationkey";
+            var userToken = "aniceusertoken";
+
+            var authenticator = new TrelloAuthenticator(applicationKey, userToken);
+
+            var _mockRestClient = new Mock<IRestClient>();
+            var _mockRestRequest = new Mock<IRestRequest>();
+
+            // Act
+            authenticator.Authenticate(_mockRestClient.Object, _mockRestRequest.Object);
+
+            // Assert
+            _mockRestRequest.Verify(x => x.AddParameter("key", applicationKey), Times.Once);
+            _mockRestRequest.Verify(x => x.AddParameter("token", userToken), Times.Once);
+        }
+    }
+}
